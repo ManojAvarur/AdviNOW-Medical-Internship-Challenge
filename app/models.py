07a5f305d287c3dd2d5f5404bc75ede751752a6a
Manoj A M <manojavarur@gmail.com>
2024-10-23 03:12:59 -0700

    Created models

--- app/models.py
+++ app/models.py
@@ -1,15 +1,40 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import mapped_column
 import datetime
+import uuid
 
 Base = declarative_base()
 
+# Holds business details
+class Business(Base):
+    __tablename__   = "business"
+    id              = Column(Integer, primary_key=True)
+    name            = Column(String(100), nullable=False)
+    created_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    updated_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    created_by      = Column(String(100), nullable=True, default="Developer")
+    updated_by      = Column(String(100), nullable=True, default="Developer")
 
-class Example(Base):
-    __tablename__    = "business"
-    id               = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    example_column   = Column(String(100), nullable=False)
-    created_at       = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.utcnow)
-    updated_at       = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.utcnow)
-    created_by       = Column(String(100), nullable=True)
-    updated_by       = Column(String(100), nullable=True)
+# Holds Symptoms data
+class Symptoms(Base):
+    __tablename__   = "symptoms"
+    id              = Column(String(100), primary_key=True)
+    name            = Column(String(100), nullable=False)
+    created_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    updated_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    created_by      = Column(String(100), nullable=True, default="Developer")
+    updated_by      = Column(String(100), nullable=True, default="Developer")
+
+# Holds business diagnostic data
+class Diagnostics(Base):
+    __tablename__   = "diagnostics"
+    id              = Column(String(100), primary_key=True, default=uuid)
+    business_id     = mapped_column(ForeignKey("business.id"), nullable=False)
+    symptom_id      = mapped_column(ForeignKey("symptoms.id"), nullable=False)
+    Diagnostics     = Column(Boolean, nullable=False)
+    # business_id     = Column(Integer, ForeignKey("business.id", ))
+    created_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    updated_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
+    created_by      = Column(String(100), nullable=True, default="Developer")
+    updated_by      = Column(String(100), nullable=True, default="Developer")

137024b756369228c9ba4c2189762092826cb9f2
Manoj A M <manojavarur@gmail.com>
2024-11-30 14:03:50 -0700

    Fixed Data Controller and also added update functionality to business

--- app/data_controller.py
+++ app/data_controller.py
@@ -13,157 +13,47 @@ class ValueError(Exception):
     """Custom Exception"""
     ...
 
-# class DataController:
-#     # Holds DB Queried Data
-#     _business_db_data:     Sequence[Row[Business]]     = None
-#     _symptoms_db_data:     Sequence[Row[Symptoms]]     = None
-#     _diagnostics_db_data:  List[str]                   = None
-
-#     _business_data_to_proc: Dict[Types, Dict[str, Business]]         = { Types.INSERT: {}, Types.UPDATE: {} }
-
-#     _symptoms_data_to_proc: Dict[Types, Dict[str, Symptoms]]         = { Types.INSERT: {}, Types.UPDATE: {} }
-
-#     _diagnostics_data_to_proc: Dict[Types, Dict[str, Diagnostics]]   = { Types.INSERT: {}, Types.UPDATE: {} }
-
-#     def __init__(self, db: Session):
-#         if not db:
-#             raise ValueError("DB Connection not found!")
-        
-#         self._business_db_data       = db.execute(Select(Business.id).order_by(Business.id)).all()
-#         self._symptoms_db_data       = db.execute(Select(Symptoms.id).order_by(Symptoms.id)).all()
-#         diagnostics_db_data          = db.execute(Select(Diagnostics.id, Diagnostics.business_id, Diagnostics.symptom_id))
-
-#         self._diagnostics_db_data = []
-#         for row in diagnostics_db_data:
-#             self._diagnostics_db_data.append(self.create_diagnostics_hash(row))
-
-#     def add_data(self, value: Business | Symptoms | Diagnostics) -> None:
-#         """
-#             1. If the data already exists in DB, it will be set to update state
-#             2. If the data is in insert state, set this also to update state
-#         """
-#         if isinstance(value, Diagnostics):
-#             self.diagnostics_data_processor(value)
-#             return
-
-#         self.business_or_symptoms_data_processor(value)
-
-
-#     def check_id_exists_in_db(self, 
-#                             id: str, 
-#                             db_data: Sequence[Row[Business]] | 
-#                                         Sequence[Row[Symptoms]] | 
-#                                         List[str]
-#     ) -> bool:
-#         return id in db_data
-    
-
-#     def check_id_exists_in_cur_ins(self, 
-#                                 value: Business | Symptoms, 
-#                                 cur_proc_data: Dict[Types, Dict[str, Business]] | 
-#                                                 Dict[Types, Dict[str, Symptoms]] | 
-#                                                 Dict[Types, Dict[str, Diagnostics]]
-#     ) -> bool:
-#         return value.id in cur_proc_data[Types.INSERT]
-
-#     def business_or_symptoms_data_processor(self, value: Business | Symptoms):
-#         data_to_proc = None
-#         db_data = None
-
-#         if isinstance(value, Business):
-#             data_to_proc = self._business_data_to_proc
-#             db_data = self._business_db_data
-#         else:
-#             data_to_proc = self._symptoms_data_to_proc
-#             db_data = self._symptoms_db_data
-
-#         if self.check_id_exists_in_db(value.id, db_data) or self.check_id_exists_in_cur_ins(value, data_to_proc):
-#             data_to_proc[Types.UPDATE][value.id] = value
-#             return
-        
-#         data_to_proc[Types.INSERT][value.id] = value
-
-#     def diagnostics_data_processor(self, value):
-#         diagnostic_hash = self.create_diagnostics_hash(value)
-
-#         if self.check_id_exists_in_db(diagnostic_hash, self._diagnostics_db_data) or \
-#             self.check_id_exists_in_cur_ins(value, self._diagnostics_data_to_proc):
-#             self._diagnostics_data_to_proc[Types.UPDATE][value.id] = value
-#             return
-        
-#         self._diagnostics_data_to_proc[Types.INSERT][diagnostic_hash] = value
-        
-#     def create_diagnostics_hash(self, diagnostic: Diagnostics) -> str:
-#         return f"{diagnostic.business_id}-{diagnostic.symptom_id}-{diagnostic.Diagnostics}"
-
-#     def upsert_business_data(self):
-#         print("\n\n-------------------------------------")
-
-#         print("--------Business For Insert----------")
-#         for i, (k, v) in enumerate(self._business_data_to_proc[Types.INSERT].items()):
-#             print(i, k, v)
-
-#         print("--------Business For Update----------")
-#         for i, (k, v) in enumerate(self._business_data_to_proc[Types.UPDATE].items()):
-#             print(i, k, v)
-
-#         print("\n\n-------------------------------------")
-
-
-#     def upsert_symptoms_data(self):
-#         print("\n\n-------------------------------------")
-#         print("\n\n--------Symptoms For Insert----------")
-
-#         for i, (k, v) in enumerate(self._symptoms_data_to_proc[Types.INSERT].items()):
-#             print(i, k, v)
-
-#         print("--------Symptoms For Update----------")
-#         for i, (k, v) in enumerate(self._symptoms_data_to_proc[Types.UPDATE].items()):
-#             print(i, k, v)
-
-#         print("\n\n-------------------------------------")
-
-
-#     def upsert_diagnostics_data(self):
-#         print("\n\n-------------------------------------")
-
-#         print("\n\n--------Diagnostics For Insert----------")
-#         for i, (k, v) in enumerate(self._diagnostics_data_to_proc[Types.INSERT].items()):
-#             print(i, k, v)
-
-#         print("--------diagnostics For Update----------")
-#         for i, (k, v) in enumerate(self._diagnostics_data_to_proc[Types.UPDATE].items()):
-#             print(i, k, v)
-
-#         print("\n\n-------------------------------------")
-        
-
-
-
-
 class DataController:
     # Holds DB Queried Data
     _business_db_data:     Tuple[str]   = None  # Holds Business Ids from DB
     _symptoms_db_data:     Tuple[str]   = None  # Holds Symptoms Ids from DB
-    _diagnostics_db_data:  Tuple[str]   = None  # Holds Combination of Business, Symptom,   Ids from DB
+    _diagnostics_db_data:  Tuple[str]   = None  # Holds Combination of Business, Symptom & Diagnostics Ids from DB
 
-    _business_data_to_proc: Dict[Types, Dict[str, Business]]         = { Types.INSERT: {}, Types.UPDATE: {} }
-    _symptoms_data_to_proc: Dict[Types, Dict[str, Symptoms]]         = { Types.INSERT: {}, Types.UPDATE: {} }
-    _diagnostics_data_to_proc: Dict[Types, Dict[str, Diagnostics]]   = { Types.INSERT: {}, Types.UPDATE: {} }
+    # Holds data which are to be processed in either INSERT / UPDATE state
+    _business_data_to_proc: Dict[Types, Dict[str, Business]]         
+    _symptoms_data_to_proc: Dict[Types, Dict[str, Symptoms]] 
+    _diagnostics_data_to_proc: Dict[Types, Dict[str, Diagnostics]] 
 
     def __init__(self, db: Session):
+        """
+        Sets DB instance and retrives all the Ids from DB
+
+        Args:
+            db: Database connected session 
+        """
         if not db:
             raise ValueError("DB Connection not found!")
         
+        # Storing DB session for future use
         self.db = db
+
+        # Instantiating Data Process Values
+        self._business_data_to_proc     = { Types.INSERT: {}, Types.UPDATE: {} }
+        self._symptoms_data_to_proc     = { Types.INSERT: {}, Types.UPDATE: {} }
+        self._diagnostics_data_to_proc  = { Types.INSERT: {}, Types.UPDATE: {} }
+
+        # Fetching currently exsiting data from DB
         self._business_db_data       = tuple(row[0] for row in db.execute(Select(Business.id).order_by(Business.id)))
         self._symptoms_db_data       = tuple(row[0] for row in db.execute(Select(Symptoms.id).order_by(Symptoms.id)))
-        self._diagnostics_db_data    = tuple(self.create_diagnostics_hash(row[0]) for row in db.execute(Select(Diagnostics.id, Diagnostics.business_id, Diagnostics.symptom_id, Diagnostics.Diagnostics)))
+        self._diagnostics_db_data    = tuple(self.create_diagnostics_hash(row) for row in db.execute(Select(Diagnostics.id, Diagnostics.business_id, Diagnostics.symptom_id, Diagnostics.Diagnostics)))
+
+        # print("business_db_data", self._business_db_data)
+        # print("_symptoms_db_data", self._symptoms_db_data)
+        # print("_diagnostics_db_data", self._diagnostics_db_data)
 
     def add_data(self, value: Business | Symptoms | Diagnostics) -> None:
         """
-            1. If the data already exists in DB, it will be set to update state
-            2. If the data is in insert state, set this also to update state
+        Appends value to list based on the type of data
         """
         if isinstance(value, Diagnostics):
             self.diagnostics_data_processor(value)
@@ -172,6 +62,17 @@ class DataController:
         self.business_or_symptoms_data_processor(value)
 
     def check_id_exists_in_db(self, id: str, db_data: Tuple[str]) -> bool:
+        """
+        Checks if the id is already in databse
+
+        Args:
+            id:      Id which has to be compared 
+            db_data: Value to which the Id has to be compared against
+
+        Returns:
+            bool: Returns True, if the value is already in DB
+        """
+        # print(f"\n\n{type(id)} {id} in {db_data}: {id in db_data}\n\n")
         return id in db_data
     
     def check_id_exists_in_cur_ins(self, 
@@ -180,9 +81,28 @@ class DataController:
                                                 Dict[Types, Dict[str, Symptoms]] | 
                                                 Dict[Types, Dict[str, Diagnostics]]
     ) -> bool:
+        """
+        Checks if the id is already under INSERT state processing
+
+        Args:
+            value:          Value of which the Id has to be compared 
+            cur_proc_data:  Value to which the Id has to be compared against
+
+        Returns:
+            bool: Returns True, if the value is already in processing state
+        """
         return value.id in cur_proc_data[Types.INSERT]
 
-    def business_or_symptoms_data_processor(self, value: Business | Symptoms):
+    def business_or_symptoms_data_processor(self, value: Business | Symptoms) -> None:
+        """
+        Check instance of value for either Business / Symptoms
+            1. If the data already exists in DB, it will be set to update state
+            2. If the data is in insert state, set this also to update state
+
+        Args:
+            value: Value to be processed
+        """
+
         data_to_proc = None
         db_data = None
 
@@ -199,7 +119,14 @@ class DataController:
         
         data_to_proc[Types.INSERT][value.id] = value
 
-    def diagnostics_data_processor(self, value):
+    def diagnostics_data_processor(self, value: Diagnostics) -> None:
+        """
+        1. If the data already exists in DB, it will be set to update state
+        2. If the data is in insert state, set this also to update state
+
+        Args:
+            value: Value to be processed
+        """
         diagnostic_hash = self.create_diagnostics_hash(value)
 
         if self.check_id_exists_in_db(diagnostic_hash, self._diagnostics_db_data) or \
@@ -210,23 +137,38 @@ class DataController:
         self._diagnostics_data_to_proc[Types.INSERT][diagnostic_hash] = value
         
     def create_diagnostics_hash(self, diagnostic: Diagnostics) -> str:
+        """
+        Generates a unique hash from Diagnostics data
+
+        Args:
+            diagnostic: Diagnostics data
+
+        Returns:
+            str: Combination of bussiness, symptom and diagnostic data
+        """
         return f"{diagnostic.business_id}-{diagnostic.symptom_id}-{diagnostic.Diagnostics}"
 
-    def upsert_business_data(self):
+    def upsert_business_data(self) -> None:
+        """
+        Inserts and Updates business data which are under processing
+        """
+
         if len(self._business_data_to_proc[Types.INSERT].values()) > 0:
             self.db.bulk_save_objects(self._business_data_to_proc[Types.INSERT].values())
             self.db.commit()
 
+        mappings = []
         for row in self._business_data_to_proc[Types.UPDATE].values():
-            value = {}
-
-            if row.name:
-                value['name'] = row.name
-
+            row.updated_at = datetime.datetime.now()
+            mappings.append(row.to_dict())
             
-            print(Update(Business).where(Business.id == row.id).values(row))
+        self.db.bulk_update_mappings(Business, mappings)
+        self.db.commit()
 
     def upsert_symptoms_data(self):
+        """
+        Inserts and Updates symptoms data which are under processing
+        """
         if len(self._symptoms_data_to_proc[Types.INSERT].values()) > 0:
             self.db.bulk_save_objects(self._symptoms_data_to_proc[Types.INSERT].values())
             self.db.commit()
@@ -246,6 +188,9 @@ class DataController:
 
 
     def upsert_diagnostics_data(self):
+        """
+        Inserts and Updates diagnostics data which are under processing
+        """
         if len(self._diagnostics_data_to_proc[Types.INSERT].values()) > 0:
             self.db.bulk_save_objects(self._diagnostics_data_to_proc[Types.INSERT].values())
             self.db.commit()
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapped_column
+from typing import Dict
 import datetime
 import uuid
 
@@ -9,7 +10,7 @@ Base = declarative_base()
 # Holds business details
 class Business(Base):
     __tablename__   = "business"
-    id              = Column(String(100), primary_key=True)
+    id              = Column(VARCHAR(100), primary_key=True)
     name            = Column(String(100), nullable=False)
     created_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
     updated_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
@@ -22,11 +23,34 @@ class Business(Base):
             f"created_at='{self.created_at}', updated_at='{self.updated_at}', "
             f"created_by='{self.created_by}', updated_by='{self.updated_by}')>"
         )
+    
+    def to_dict(self, add_none: bool = False) -> Dict[str, any]:
+        mapping_dict = {}
+
+        if self.id != None or add_none:
+            mapping_dict['id'] = self.id
+
+        if self.name != None or add_none:
+            mapping_dict['name'] = self.name
+
+        if self.created_at != None or add_none:
+            mapping_dict['created_at'] = self.created_at
+
+        if self.updated_at != None or add_none:
+            mapping_dict['updated_at'] = self.updated_at
+
+        if self.created_by != None or add_none:
+            mapping_dict['created_by'] = self.created_by
+
+        if self.updated_by != None or add_none:
+            mapping_dict['updated_by'] = self.updated_by
+
+        return mapping_dict
 
 # Holds Symptoms data
 class Symptoms(Base):
     __tablename__   = "symptoms"
-    id              = Column(String(100), primary_key=True)
+    id              = Column(VARCHAR(100), primary_key=True)
     name            = Column(String(100), nullable=False)
     created_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
     updated_at      = Column(DateTime(timezone=True), nullable=False, default=datetime.datetime.now)
@@ -39,11 +63,34 @@ class Symptoms(Base):
             f"created_at='{self.created_at}', updated_at='{self.updated_at}', "
             f"created_by='{self.created_by}', updated_by='{self.updated_by}')>"
         )
+    
+    def to_dict(self, add_none: bool = False) -> Dict[str, any]:
+        mapping_dict = {}
+
+        if self.id != None or add_none:
+            mapping_dict['id'] = self.id
+
+        if self.name != None or add_none:
+            mapping_dict['name'] = self.name
+
+        if self.created_at != None or add_none:
+            mapping_dict['created_at'] = self.created_at
+
+        if self.updated_at != None or add_none:
+            mapping_dict['updated_at'] = self.updated_at
+
+        if self.created_by != None or add_none:
+            mapping_dict['created_by'] = self.created_by
+
+        if self.updated_by != None or add_none:
+            mapping_dict['updated_by'] = self.updated_by
+
+        return mapping_dict
 
 # Holds business diagnostic data
 class Diagnostics(Base):
     __tablename__   = "diagnostics"
-    id              = Column(String(100), primary_key=True, default=uuid.uuid4)
+    id              = Column(VARCHAR(100), primary_key=True, default=uuid.uuid4)
     business_id     = mapped_column(ForeignKey("business.id"), nullable=False)
     symptom_id      = mapped_column(ForeignKey("symptoms.id"), nullable=False)
     Diagnostics     = Column(Boolean, nullable=False)
@@ -58,3 +105,32 @@ class Diagnostics(Base):
             f"created_at='{self.created_at}', updated_at='{self.updated_at}', "
             f"created_by='{self.created_by}', updated_by='{self.updated_by}')>"
         )
+    
+    def to_dict(self, add_none: bool = False) -> Dict[str, any]:
+        mapping_dict = {}
+
+        if self.id != None or add_none:
+            mapping_dict['id'] = self.id
+
+        if self.business_id != None or add_none:
+            mapping_dict['business_id'] = self.business_id
+
+        if self.symptom_id != None or add_none:
+            mapping_dict['symptom_id'] = self.symptom_id
+
+        if self.Diagnostics != None or add_none:
+            mapping_dict['Diagnostics'] = self.Diagnostics
+
+        if self.created_at != None or add_none:
+            mapping_dict['created_at'] = self.created_at
+
+        if self.updated_at != None or add_none:
+            mapping_dict['updated_at'] = self.updated_at
+
+        if self.created_by != None or add_none:
+            mapping_dict['created_by'] = self.created_by
+
+        if self.updated_by != None or add_none:
+            mapping_dict['updated_by'] = self.updated_by
+
+        return mapping_dict
